6e87794c66c83de9c19dad3e822c57d3f918d2d1
jsladovic <janko.sladovic@gmail.com>
2020-04-18 11:32:58 +0200

    initial commit

---
+++ Assets/Scripts/GuardController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardController : MonoBehaviour
+{
+	public CharacterController Controller;
+	public Transform PathParent;
+	private Transform[] Path;
+
+	private const float Speed = 2.0f;
+
+	private int CurrentPathNode;
+	private Transform CurrentPathTransform => Path[CurrentPathNode];
+
+    void Start()
+    {
+		Path = new Transform[PathParent.childCount];
+		for(int i = 0; i < PathParent.childCount; i++)
+		{
+			Path[i] = PathParent.GetChild(i);
+		}
+
+		CurrentPathNode = 0;
+	}
+
+    void Update()
+    {
+		Vector3 direction = CurrentPathTransform.position - transform.position;
+		direction.y = 0.0f;
+		direction.Normalize();
+
+		Controller.Move(direction * Speed * Time.deltaTime);
+		transform.rotation = Quaternion.LookRotation(direction);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.transform == CurrentPathTransform)
+		{
+			CurrentPathNode = (CurrentPathNode + 1) % Path.Length;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public CharacterController Controller;
+	private const float Speed = 2.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+	{
+		Vector3 direction = Vector3.zero;
+		if (Input.GetAxisRaw("Horizontal") != 0.0f)
+		{
+			direction.x = Input.GetAxisRaw("Horizontal");
+		}
+		if (Input.GetAxisRaw("Vertical") != 0.0f)
+		{
+			direction.z = Input.GetAxisRaw("Vertical");
+		}
+		if (direction != Vector3.zero)
+		{
+			direction.Normalize();
+			transform.rotation = Quaternion.LookRotation(direction);
+			Controller.Move(direction * Speed * Time.deltaTime);
+		}
+	}
+}
