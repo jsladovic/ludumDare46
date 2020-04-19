81189bb9e11fbd086538877c3fb45a3d1d8c357c
jsladovic <janko.sladovic@gmail.com>
2020-04-19 11:47:16 +0200

    Created basic level selector on main menu

---
+++ Assets/Scripts/UI/LevelSelector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+	public SceneLoader SceneLoader;
+
+	public void SelectLevel(int index)
+	{
+		SceneLoader.LoadScene(index);
+	}
+}
