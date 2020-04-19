04b3189cbce92c05827956dea247ba5e3ddc2f60
jsladovic <janko.sladovic@gmail.com>
2020-04-19 11:32:55 +0200

    Added Level Selector scene, TMPro, Scene Loader script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+	public Slider PercentageSlider;
+
+	public void LoadScene(int index)
+	{
+		StartCoroutine(LoadAsynchronously(index));
+	}
+
+	private IEnumerator LoadAsynchronously(int index)
+	{
+		if (PercentageSlider != null)
+			PercentageSlider.gameObject.SetActive(true);
+
+		AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+		while (operation.isDone == false)
+		{
+			float progress = Mathf.Clamp01(operation.progress / 0.9f);
+			if (PercentageSlider != null)
+				PercentageSlider.value = (int)progress;
+
+			yield return null;
+		}
+
+		if (PercentageSlider != null)
+			PercentageSlider.gameObject.SetActive(false);
+	}
+}
