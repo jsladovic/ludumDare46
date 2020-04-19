e4e2f82964eca7b95c4fbda5f9c507d31c317a03
jsladovic <janko.sladovic@gmail.com>
2020-04-19 23:23:36 +0200

    Implemented basic main menu functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,15 @@ public class GameManager : MonoBehaviour
 {
 	public static GameManager Instance;
 	public bool Alarmed { get; private set; }
+	public bool Paused { get; private set; }
+
+	public Canvas PauseMenuCanvas;
 
 	private void Awake()
 	{
 		Instance = this;
 		Alarmed = false;
+		PauseMenuCanvas.enabled = false;
 	}
 
 	void Update()
@@ -23,7 +27,39 @@ public class GameManager : MonoBehaviour
 		{
 			LowerSecurityAwareness();
 		}
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			TogglePause();
+		}
     }
+
+	private void TogglePause()
+	{
+		Paused = !Paused;
+		if (Paused == true)
+		{
+			// Pausing game
+			Time.timeScale = 0.0f;
+			PauseMenuCanvas.enabled = true;
+		}
+		if (Paused == false)
+		{
+			// Resuming game
+			Time.timeScale = 1.0f;
+			PauseMenuCanvas.enabled = false;
+		}
+	}
+
+	public void ResumeGame()
+	{
+		if (Paused == true)
+			TogglePause();
+	}
+
+	public void BackToMainMenu()
+	{
+		LevelManager.Instance.SceneLoader.LoadScene(0);
+	}
 
 	private void RaiseSecurityAwareness()
 	{
