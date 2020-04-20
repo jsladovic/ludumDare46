a8b02e3312db170edb344166db18d89d773bc688
jsladovic <janko.sladovic@gmail.com>
2020-04-20 22:03:13 +0200

    Added level 2

---
+++ Assets/Scripts/Levels/Level2Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Level2Controller : MonoBehaviour
+{
+	void Start()
+	{
+		StartCoroutine(DisplayIntroMessage());
+	}
+
+	private IEnumerator DisplayIntroMessage()
+	{
+		yield return new WaitForSeconds(1.0f);
+		GameManager.Instance.DisplayMessage("Right, this time take the right at the intersection and we'll find Hutchinson", MessageSource.Driver);
+	}
+}
